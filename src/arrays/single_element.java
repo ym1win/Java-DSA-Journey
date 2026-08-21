3c6affa935f3d02d8bef64ead550088f0f27ea88
Krish Maurya <yashmaurya370@gmail.com>
2026-08-21 19:46:32 +0530

    solved array dsa question

---
+++ src/arrays/single_element.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class single_element {
+
+    public static void main(String[] args) {
+
+        int[] nums = {4, 1, 2, 1, 2};
+
+        int ans = 0;
+
+        for (int n : nums) {
+            ans ^= n;
+        }
+
+        System.out.println("Single element: " + ans);
+    }
+}
