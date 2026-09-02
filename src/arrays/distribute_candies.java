f705dc13a340bb271b8468a0f361a714f8d0bdab
Krish Maurya <yashmaurya370@gmail.com>
2026-09-02 22:02:05 +0530

    optimized array dsa question

--- src/arrays/distribute_candies.java
+++ src/arrays/distribute_candies.java
@@ -13,7 +13,6 @@ public class distribute_candies {
 
         System.out.println("Maximum different candy types: " + result);
     }
-
     public static int distributeCandies(int[] candyType) {
 
         Set<Integer> types = new HashSet<>();
