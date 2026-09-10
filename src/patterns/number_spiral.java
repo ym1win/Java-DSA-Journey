85189c62a97da43ef2d3e460b9292045e2f51d97
Krish Maurya <yashmaurya370@gmail.com>
2026-09-10 13:41:07 +0530

    number spiral pattern

--- src/patterns/number_spiral.java
+++ src/patterns/number_spiral.java
@@ -23,7 +23,6 @@ public class number_spiral {     // pehle 1/4 th print krke smjhenge then
                 System.out.print(Math.min(a,b)+" ");
             }
             System.out.println();
-
         }
     }
 }
