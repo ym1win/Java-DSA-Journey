6c50848faab85b88a17231c4f2570873c65f83ee
Krish Maurya <yashmaurya370@gmail.com>
2026-09-24 12:10:31 +0530

    revision dsa

--- src/patterns/floyds_triangle.java
+++ src/patterns/floyds_triangle.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class floyds_triangle {
     static void main() {
         Scanner sc=new Scanner(System.in);
+
         System.out.println("Enter no of rows");
         int rows=sc.nextInt();
         int a=1;
